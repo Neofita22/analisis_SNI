8d66a010e24de969ec80f39020e83a1b95febb91
bendezs <stacy.sayas@gmail.com>
2024-04-07 15:51:19 -0400

    datos de DR. DrAs actualizados

--- data/01-clean.R
+++ data/01-clean.R
@@ -1,11 +1,13 @@
 install.packages("pacman")
+
+#load & install, if you dont have this pckgs
 pacman::p_load("vroom", "dplyr")
 
 #alt+ "-" == <-
 la_data <- vroom(file = "/home/bendezs/Clacesita/proyecto_erecita/analisis_SNI/data/data_original.csv")
 
-
-# ==%>%
+#chmod u+w filename, dar permisos de modificar a un user!
+# Ctrl + Shift + M == %>% , m o M da igual
 
 data_filtrada <- la_data %>% 
   filter(nobilis =="DR." | nobilis == "DRA.")
@@ -14,6 +16,9 @@ data_filtrada <- la_data %>%
 #getwd()
 ##corregir el path del directorio
 #setwd("/home/bendezs/Clacesita/proyecto_erecita/analisis_SNI/")
+
+# crea un archivo vacio o lo actualiza en el tiempo
+#<touch filename/file.R>
 
 # creamos un directorio de resultados
 dir.create( path = "/home/bendezs/Clacesita/proyecto_erecita/analisis_SNI/results" )
@@ -38,16 +43,6 @@ write.csv( x = data_resumen, file = "results/resumen.csv" )
 #           como TOTAL INGRESOS / TOTAL PERDIDAS
 
 # SON BUENAS PRACTICAS DEL CODIGO PARA VER LOS PENDIENTES #
-
-
-
-
-
-
-
-
-
-
 
 
 
